54065683766ea9dca2e263e04e8620f4b344a31b
Tal <talkanteman@gmail.com>
2019-02-08 19:01:53 +0200

    Updates

---
+++ precache-manifest.c22f573cc323199990281b4280226023.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b15b148a7298bc795a520f2a0ecad80",
+    "url": "/whist/static/media/heart.2b15b148.png"
+  },
+  {
+    "revision": "d467c9562c754ec7c4c09eb8a67f359c",
+    "url": "/whist/static/media/diamond.d467c956.png"
+  },
+  {
+    "revision": "8da87cd9bb7b08f04efc33388c82002a",
+    "url": "/whist/static/media/cards_back.8da87cd9.png"
+  },
+  {
+    "revision": "39ba10689132f3a85712f221df6a9622",
+    "url": "/whist/static/media/background.39ba1068.jpg"
+  },
+  {
+    "revision": "1c5596017241c318e8a6d5f14e96a38a",
+    "url": "/whist/static/media/Club.1c559601.png"
+  },
+  {
+    "revision": "bc5f1a0ce49247ecab55",
+    "url": "/whist/static/js/runtime~main.bc5f1a0c.js"
+  },
+  {
+    "revision": "8ac3bbff200cb5f2a6ba",
+    "url": "/whist/static/js/main.8ac3bbff.chunk.js"
+  },
+  {
+    "revision": "46e7d8de42c1bdf346ab",
+    "url": "/whist/static/js/1.46e7d8de.chunk.js"
+  },
+  {
+    "revision": "8ac3bbff200cb5f2a6ba",
+    "url": "/whist/static/css/main.c83e0f2a.chunk.css"
+  },
+  {
+    "revision": "a0f006a2ee4a7ead55d85188007af70a",
+    "url": "/whist/index.html"
+  }
+];
